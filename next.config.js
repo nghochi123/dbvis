57f8184596ae8b0aa50484012ab096d221c03271
nghochi123 <nghochi123@gmail.com>
2021-04-22 19:58:04 +0800

    trying to solve the deploy problem again

--- next.config.js
+++ next.config.js
@@ -2,12 +2,15 @@ module.exports = {
     // Target must be serverless
     target: 'serverless',
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      config.plugins.push(new webpack.IgnorePlugin(/^oracle$/));
-      config.plugins.push(new webpack.IgnorePlugin(/^oracledb$/));
-      config.plugins.push(new webpack.IgnorePlugin(/^postgres$/));
-      config.plugins.push(new webpack.IgnorePlugin(/^redshift$/));
-      config.plugins.push(new webpack.IgnorePlugin(/^tedious$/));
-      config.plugins.push(new webpack.IgnorePlugin(/^sqlite3$/))
+        new webpack.IgnorePlugin(/mariasql/, /\/knex\//),
+        new webpack.IgnorePlugin(/mssql/, /\/knex\//),
+        new webpack.IgnorePlugin(/postgres/, /\/knex\//),
+        new webpack.IgnorePlugin(/oracle/, /\/knex\//),
+        new webpack.IgnorePlugin(/oracledb/, /\/knex\//),
+        new webpack.IgnorePlugin(/pg-query-stream/, /\/knex\//),
+        new webpack.IgnorePlugin(/sqlite3/, /\/knex\//),
+        new webpack.IgnorePlugin(/strong-oracle/, /\/knex\//),
+        new webpack.IgnorePlugin(/pg-native/, /\/pg\//)
       return config
     },
   };
