1d11cb621a2cc2cfa7c621c5d74c53e95f49fab6
nghochi123 <nghochi123@gmail.com>
2021-04-22 20:06:52 +0800

    again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    // Target must be serverless
-    target: 'serverless',
+
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
         new webpack.IgnorePlugin(/mariasql/, /\/knex\//),
         new webpack.IgnorePlugin(/mssql/, /\/knex\//),
@@ -9,8 +8,7 @@ module.exports = {
         new webpack.IgnorePlugin(/oracledb/, /\/knex\//),
         new webpack.IgnorePlugin(/pg-query-stream/, /\/knex\//),
         new webpack.IgnorePlugin(/sqlite3/, /\/knex\//),
-        new webpack.IgnorePlugin(/strong-oracle/, /\/knex\//),
-        new webpack.IgnorePlugin(/pg-native/, /\/pg\//)
+        new webpack.IgnorePlugin(/strong-oracle/, /\/knex\//)
       return config
     },
   };
