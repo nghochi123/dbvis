228df4047c8f5dfc8ee56d697bb77ca6e9a61caa
nghochi123 <nghochi123@gmail.com>
2021-04-22 21:50:42 +0800

    /

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([
-    [optimizedImages, {
+module.exports = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
         /* config for next-optimized-images */
-      }],
-], {
-    distDir: 'out',
+      },
+    ],
+  ],
+  {
+    target: "serverless",
+    distDir: "out",
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.plugins.push(
         new webpack.IgnorePlugin(/mariasql/, /\/knex\//),
         new webpack.IgnorePlugin(/mssql/, /\/knex\//),
         new webpack.IgnorePlugin(/postgres/, /\/knex\//),
@@ -16,6 +23,8 @@ module.exports = withPlugins([
         new webpack.IgnorePlugin(/pg-query-stream/, /\/knex\//),
         new webpack.IgnorePlugin(/sqlite3/, /\/knex\//),
         new webpack.IgnorePlugin(/strong-oracle/, /\/knex\//)
-      return config
+      );
+      return config;
     },
-    })
+  }
+);
